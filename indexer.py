ba09be8d5ba9fd5afe4cb0aaadb131b89a2214e4
levAriUlnerWork <levariulner@gmail.com>
2020-11-23 11:15:50 +0200

    indexer fixed

--- indexer.py
+++ indexer.py
@@ -20,7 +20,7 @@ class Indexer:
         localDocPostingFile = {}
         listOfUniques = [] #list of unique terms
         self.term_max_freq[docID] = [] #[[] , , []]
-        # Go over each term in the doc d
+        # Go over each term in the doc
         for term in document_dictionary.keys():
             try:
                 #Update the freq of the term in this doc:
