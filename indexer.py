78ee7203135dfa6ea99f0f639b82530c22bd9e82
levAriUlnerWork <levariulner@gmail.com>
2020-11-24 18:46:27 +0200

    Fixed indexer

--- indexer.py
+++ indexer.py
@@ -1,4 +1,4 @@
-import heapq
+import heapq #TODO: Change to priority queue
 class Indexer:
 
     def __init__(self, config):
@@ -21,37 +21,28 @@ class Indexer:
         docID = document.tweet_id
         document_dictionary = document.term_doc_dictionary # document_dictionary = {term:[[indexes],freq]
         freq_terms = {} # save the freq of the term in this doc
-        localDocPostingFile = {}
         listOfUniques = [] #list of unique terms
         self.term_max_freq[docID] = [] #[[] , , []]
         # Go over each term in the doc
         for term in document_dictionary.keys():
             try:
-                #Update the freq of the term in this doc:
-                if(term not in freq_terms):
-                    freq_terms[term] = 1
-                    localDocPostingFile[term] = [1,docID,document_dictionary[term][0]]
-                else:
-                    freq_terms[term] += 1
-                    localDocPostingFile[term][0] +=1
+                #Update the local dicts: freq of the term in this doc:
 
-                # Update inverted index and termMax
+                freq_terms[term] = document_dictionary[term][1]
+
+                # Update the public inverted index and termMax
                 if term not in self.inverted_idx.keys():
                     self.postingDict[term] = []
-                    self.inverted_idx[term] = [1,1,self.postingDict]#TODO: Change the self.postingDict to the name of the posting file
-
+                    self.inverted_idx[term] = [1,document_dictionary[term][1],self.postingDictNames[0]]#TODO: Change the self.postingDict to the name of the posting file
 
                 else:
                     #update inv_dict:
-                    if(freq_terms[term] == 1):
-                        # update inv_dict:
-                        self.inverted_idx[term][0] += 1 # add another doc to the count in the inv_dict
-                    self.inverted_idx[term][1] += 1
+                    self.inverted_idx[term][0] += 1 # add another doc to the count in the inv_dict
+                    self.inverted_idx[term][1] += document_dictionary[term][1]
 
-                if(freq_terms[term] == document_dictionary[term][1]):#if we finished with this term in this doc, we update the posting file
-                    heapq.heappush(self.postingDict[term],localDocPostingFile[term])
-                    if(freq_terms[term] == 1):
-                        listOfUniques.append(term)
+                heapq.heappush(self.postingDict[term],[document_dictionary[term][1],docID,document_dictionary[term][0]])
+                if document_dictionary[term][1] == 1:
+                    listOfUniques.append(term)
 
 
 
